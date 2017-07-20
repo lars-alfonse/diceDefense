d355f9f0e79d8c1f69b0c7d45241d1b2116d2350
lars-alfonse <lars.alfonse@gmail.com>
2017-07-20 11:26:26 -0500

    added images to table, changed dice selection, js overhaul

--- diceGame.js
+++ diceGame.js
@@ -2,135 +2,132 @@
 function add(sum, value){
 	return sum + value;
 }
-function uniqueDice(value, index, self){
-	return self.indexOf(value) === index;
-}
-function diceAllocation(){
-	var diceReserve;
-	diceReserve = [20, 12, 12, 10, 10, 8, 8, 6, 6, 6, 4, 4, 4]
-	var attackDice;
-	var currentDice;
-	var defenseDice;
-	var speedDice;
-	attackDice = [];
-	defenseDice = [];
-	speedDice = [];
-	var userDieChoice;
-	while (diceReserve.length > 0) {
-		userDieChoice = prompt("You currently have the following dice " + diceReserve + " where would you like the next die to go?" , "defense, speed, attack");
-		if (userDieChoice === "Defense" || userDieChoice === "defense"){
-			defenseDice.push(diceReserve[diceReserve.length - 1]);
-			diceReserve.pop();
-		}
-		else if (userDieChoice === "attack" || userDieChoice === "Attack"){
-			attackDice.push(diceReserve[diceReserve.length - 1]);
-			diceReserve.pop();
-		}
-		else if(userDieChoice === "Speed" || userDieChoice === "speed"){
-			speedDice.push(diceReserve[diceReserve.length - 1]);
-			diceReserve.pop();
-		}
-		else {
-			alert("incorrect Entry");
-		}
-	}
-	defenseDice = defenseDice.join(" ");
-	attackDice = attackDice.join(" ");
-	speedDice = speedDice.join(" ");
-	currentDice =  defenseDice + "/" + attackDice + "/" + speedDice;
-	return currentDice;
-}
-function determineStats(){
-
-}
-function diceRoll(dieValue, frequency){
+function diceRoll(dieValue){
 	var dieResult;
-	var diceResult;
-	diceResult = [];
-	for (var i = 0; i < frequency; i++) {
-		dieResult = Math.floor(Math.random()*dieValue + 1);
-		if (dieResult === dieValue){
-			dieResult = dieResult*1.5;
-		}
-		console.log(dieResult);
-		diceResult.push(dieResult);
+	dieResult = Math.floor(Math.random()*dieValue + 1);
+	if (dieResult === dieValue) {
+		dieResult = dieResult * 1.5;
 	}
-	return diceResult;
-}
-function sortDiceForRoll(currentDice){
-	var diceSet;
-	var diceValue;
-	var diceCount;
-	var currentDiceValueAndCount;
-	currentDiceValueAndCount = [];
-	diceSet = currentDice.split("/");
-	for (var i = 0; i < diceSet.length; i++) {
-		
-	}
+	return dieResult;
 }
 function addDice(diceResult){
 	var diceTotal;
+	diceResult = diceResult.concat(0);
 	diceTotal = diceResult.reduce(add);
 	return diceTotal;
 }
-function countDice(diceSet, dieValue) {
-	var diceAmount;
-	var diceCount;
-	diceAmount = diceSet.length;
-	diceCount  = 0;
-	for (var i = 0; i < diceSet.length; i++) {
-		if (diceSet[i] === dieValue || diceSet[i]+diceSet[i+1] === dieValue){
-			diceCount += 1;
+function reportStats(){
+
+}
+function collectDiceResults(diceSet){
+		var diceResults;
+		var dieResult;
+		diceResults = [];
+		for (var i = 0; i < diceSet.length; i++) {
+			dieResult = diceRoll(diceSet[i]);
+			console.log(dieResult);
+			diceResults.push(dieResult);
+		}
+		dieResult = addDice(diceResults);
+	return dieResult;
+}
+function selectPlayerDice(player, type) {
+	var selectedDice;
+	var selectedDiceKit;
+	selectedDice = [];
+	selectedDiceKit = [];
+	var playerUser;
+	playerUser = document.getElementById('player'+player);
+	selectedDice = playerUser.getElementsByClassName(type);
+	for (var i = 0; i < selectedDice.length; i++) {
+		if(selectedDice[i].checked){
+			selectedDiceKit.push(parseInt(selectedDice[i].value, 10));
 		}
 	}
-	return diceCount;
+	return selectedDiceKit;
 }
-function separateDice(currentDice){
-	var diceSet;
-	diceSet = currentDice.split("/");
-	return diceSet;
+function startCombat(playerOneAttack, playerOneDefense, playerOneSpeed, playerTwoAttack, playerTwoDefense, playerTwoSpeed, hitPoints){
+	var playerOneInitiative;
+	var playerTwoInitiative;
+	var playerOneAttackMultiplier;
+	var playerTwoAttackMultiplier;
+	playerOneInitiative = determineInitiative(playerOneSpeed, playerTwoSpeed);
+	playerTwoInitiative = determineInitiative(playerTwoSpeed, playerOneSpeed);
+	playerOneAttackMultiplier = determineAttackMultiplier(playerOneSpeed, playerTwoSpeed);
+	playerTwoAttackMultiplier = determineAttackMultiplier(playerTwoSpeed, playerOneSpeed);
+	playerOneAttack =  playerOneAttackMultiplier * playerOneAttack;
+	playerTwoAttack = playerTwoAttackMultiplier * playerTwoAttack;
+	hitPoints = damageStep(playerOneAttack, playerOneDefense, playerOneInitiative, playerTwoAttack, playerTwoDefense, playerTwoInitiative, hitPoints);
+	return hitPoints;
 }
-function removeRepeatedDiceValues(diceSet){
-	var uniqueDiceSet;
-	uniqueDiceSet = diceSet.filter( uniqueDice );
-	return uniqueDiceSet;
-}
-function changeDiceStringToNumbers(diceSet){
-	var numberDiceArray;
-	numberDiceArray = [];
-	for (var i = 0; i < diceSet.length; i++) {
-		numberDiceArray.push(parseInt(diceSet[i], 10));
+function damageStep(playerOneAttack, playerOneDefense, playerOneInitiative, playerTwoAttack, playerTwoDefense, playerTwoInitiative, hitPoints){
+	if (playerOneInitiative > playerTwoInitiative) {
+		if (playerOneAttack > playerTwoDefense) {
+			hitPoints[1] = hitPoints[1] - 1;
+			}
+		if (hitPoints[1] === 0) {
+			return hitPoints;
+		}
+		else if(playerTwoAttack > playerOneDefense) {
+			hitPoints[0] -= 1;
+		}
 	}
-	return numberDiceArray;
-}
-function organizeDice(currentDice){
-	var separatedDice;
-	currentDice = diceAllocation();
-	separatedDice = separateDice(currentDice);
-	return separatedDice;
-}
-/*function sortDiceForRoll(separatedDice){
-	var separatedDiceSet;
-	var diceValue;
-	var diceValuesWithCounts;
-	diceValuesWithCounts = [];
-	for (var i = 0; i < separatedDice.length; i++) {
-		var separatedDiceArray;
-		separatedDiceSet = separatedDice[i];
-		console.log(separatedDiceSet);
-		var diceOptions;
-		diceOptions = [20, 12, 10, 8, 6, 4];
-		for (var k = 0; k < diceOptions.length; k++) {
-		 	var dieValue;
-		 	var dieCount;
-		 	dieValue = diceOptions[k].toString();
-		 	dieCount = countDice(separatedDiceSet, dieValue);
-		 	diceValuesWithCounts.push(dieValue,dieCount);
-		 	console.log(dieValue, dieCount);
-		 } 
+	else{
+		if (playerTwoAttack > playerOneDefense) {
+			hitPoints[0] = hitPoints[0] - 1;
+		}
+		if (hitPoints[0] ===  0) {
+			return hitPoints;
+		}
+		else if(playerOneAttack > playerTwoDefense){
+			hitPoints[1] = hitPoints[1] - 1;
+		}
 	}
-	return diceValuesWithCounts;
-}*/
-var testString;
-testString = ["4 6 8 10 20", "4 6 8 12", "4 6 10 12"];
-console.log(sortDiceForRoll(testString));
+	return hitPoints;
+}
+function determineInitiative(firstSpeed, secondSpeed){
+	var initiative;
+	firstSpeed > secondSpeed ? initiative = 1 : initiative = 0;
+	return initiative;
+}
+function determineDefender(firstAttacker){
+	var firstDefender;
+	firstAttacker === "playerOne" ? firstDefender = "playerOne" : firstDefender = "playerTwo";
+	return firstDefender;
+}
+function determineAttackMultiplier(firstSpeed, secondSpeed){
+	var attackMultiplier;
+	attackMultiplier = firstSpeed / secondSpeed;
+	attackMultiplier <= 1 ? attackMultiplier = 1 : attackMultiplier = attackMultiplier;
+	return attackMultiplier;
+}
+function startGame(){
+	var hitPoints;
+	hitPoints = [3, 3];
+	var playerOneAttack;
+	var playerOneDefense;
+	var playerOneSpeed;
+	var playerOne = "One";
+	var playerTwo = "Two";
+	while (hitPoints[0] > 0 && hitPoints[1] > 0){
+	playerOneAttack = selectPlayerDice(playerOne, "attack");
+	playerOneDefense = selectPlayerDice(playerOne, "defense");
+	playerOneSpeed = selectPlayerDice(playerOne, "speed");
+	var playerTwoAttack = selectPlayerDice(playerTwo, "attack");
+	var playerTwoDefense = selectPlayerDice(playerTwo, "defense");
+	var playerTwoSpeed = selectPlayerDice(playerTwo, "speed");
+	playerOneAttack = collectDiceResults(playerOneAttack);
+	playerOneDefense = collectDiceResults(playerOneDefense);
+	playerOneSpeed = collectDiceResults(playerOneSpeed);
+	playerTwoAttack = collectDiceResults(playerTwoAttack);
+	playerTwoDefense = collectDiceResults(playerTwoDefense);
+	playerTwoSpeed = collectDiceResults(playerTwoSpeed);
+		hitPoints = startCombat(playerOneAttack, playerOneDefense, playerOneSpeed, playerTwoAttack, playerTwoDefense, playerTwoSpeed, hitPoints);
+	}
+	if (hitPoints[0] === 0){
+		console.log("Player one loses");
+	}
+	else{
+		console.log("player two loses");
+	}
+}
